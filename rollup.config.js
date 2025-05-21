7c39958888df5536d2c1ce544ca8f8ab2a798263
Daotin <daotin@qq.com>
2025-05-21 21:16:38 +0800

    更新package-lock.json和rollup配置，调整依赖版本，添加bin配置，优化输出设置。

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,16 @@
-import { nodeResolve } from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import json from '@rollup/plugin-json';
-import { terser } from 'rollup-plugin-terser';
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import json from "@rollup/plugin-json";
+import { terser } from "rollup-plugin-terser";
 
 export default {
-  input: 'src/code-review.js',
+  input: "src/code-review.js",
   output: {
-    file: 'dist/index.js',
-    format: 'es',
-    banner: '#!/usr/bin/env node',
+    file: "dist/index.js",
+    format: "es",
+    banner: "#!/usr/bin/env node",
+    inlineDynamicImports: true,
   },
-  external: ['fs', 'path', 'url', 'child_process', 'os'],
-  inlineDynamicImports: true,
+  external: ["fs", "path", "url", "child_process", "os"],
   plugins: [nodeResolve(), commonjs(), json(), terser()],
 };
